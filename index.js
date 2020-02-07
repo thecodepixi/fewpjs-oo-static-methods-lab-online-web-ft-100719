ebf209716f1b2323d1ed472af74d3cfcb1335ec5
Emily <emily.a.harber@gmail.com>
2020-02-07 15:06:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(word){
+    let cap = word.charAt(0).toUpperCase()
+    let end = word.slice(1)
+    return cap+end 
+  }
+
+  static sanitize(string){
+    return string.replace(/(\W*)[^.'\-\w\s]/g, "")
+  }
+
+  static titleize(string){
+    let split = string.split(" ")
+    let acceptable = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let titleized = ""
+    for(let i = 0; i < split.length; i++) {
+      if( !acceptable.includes(split[i]) || split.indexOf(split[i]) === 0)
+        titleized += `${this.capitalize(split[i])} `
+      else 
+      titleized += `${split[i]} `
+    }
+    return titleized.slice(0, -1)
+  }
 }
